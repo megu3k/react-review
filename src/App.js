d5fc555a529cfb03e28ba1f75c5bafe35800f1c5
megu3k <49593780+megu3k@users.noreply.github.com>
2021-03-20 20:55:22 +0900

    use useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  console.log("App");
+  const [count, setCount] = useState(0);
+  const onClickCount = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>{count}</p>
+      <button onClick={onClickCount}>カウントアップ</button>
     </div>
   );
 }
